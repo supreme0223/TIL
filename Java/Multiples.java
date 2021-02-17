7ea6e64a2581b833d6785773346d0381b5100dc7
Jake Park <supremeletra@gmail.com>
2021-02-17 23:15:32 +0900

    Multiples Program

---
+++ Java/Multiples.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int result = 0;
+			for (int i = 1; i < 101; i++) {
+				
+				if (i%3 == 0) {
+					
+					result = result + i;
+				}
+			}
+		System.out.println("The sum of 3's multiple is " + result); 
+			
+			
+		}
+		
+		
+		
+	}
+
+
